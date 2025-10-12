a47cd8b37fe6dace13c500ce6fe71ef47a4c7169
Jesse Buhagiar <jooster669@gmail.com>
2025-10-12 12:52:21 +0000

    feat: Add basic Kernel `printf`
    This routes the output of our Kernel's `libc` straight to the debug
    Terminal so we can actually see what's going on

---
+++ kernel/kstdlib/stdio.c
@@ -0,0 +1,144 @@
+/****************************************************************
+ * @file    stdio.c
+ * @brief   Implementation of @c stdio.h
+ *
+ * @copyright SPDX-License-Identifier: GPL-3.0-or-later
+ ****************************************************************/
+
+#include <stdio.h>
+
+#include <hal.h>
+
+static const char* digits = "0123456789ABCDEF";
+
+[[gnu::always_inline]] static inline int printf_internal_Number(int number)
+{
+    int num = 0;
+
+    if (number < 0)
+    {
+        (void)srv_hal_WriteDebugChar('-');
+
+        /* Modifying the param directly is bad practice, but let's do it for now */
+        number -= number;
+    }
+
+    do
+    {
+        const char to_print  = digits[number % 10];
+        number              /= 10;
+        (void)srv_hal_WriteDebugChar(to_print);
+
+        num++;
+    } while (number != 0);
+
+    return num;
+}
+
+[[gnu::always_inline]] static inline int printf_internal_Hex(int number)
+{
+    /* Stole this from kling, but it's pretty basic */
+
+    int ret    = 0;
+    int shifts = 0;
+    for (__SIZE_TYPE__ i = number; i > 0; i >>= 4)
+    {
+        shifts++;
+    }
+
+    if (shifts == 0)
+    {
+        shifts = 1;
+    }
+
+    shifts *= 4;
+    for (int i = (32 - shifts) / 4; i > 0; i--)
+    {
+        (void)srv_hal_WriteDebugChar('0');
+    }
+
+    while (shifts > 0)
+    {
+        shifts -= 4;
+        (void)srv_hal_WriteDebugChar(digits[(number >> shifts) & 0xFU]);
+    }
+
+    return ret;
+}
+
+int printf_internal(const char* format, __builtin_va_list* va)
+{
+    __SIZE_TYPE__ index       = 0ULL;
+    int           num_written = 0;
+
+    char character = format[index];
+    while (character != '\0')
+    {
+        /* Have we encountered a format specifier? */
+        if (character == '%')
+        {
+            const char format_char = format[index + 1ULL];
+            switch (format_char)
+            {
+            case 'c':
+            {
+                const char c_to_print = __builtin_va_arg(*va, int);
+                (void)srv_hal_WriteDebugChar((char)c_to_print);
+
+                num_written++;
+                break;
+            }
+            case 'd':
+            {
+                int val      = __builtin_va_arg(*va, int);
+                num_written += printf_internal_Number(val);
+
+                if (val < 0)
+                {
+                    num_written++;
+                }
+                break;
+            }
+            case 'x':
+            {
+                uint32_t val  = __builtin_va_arg(*va, uint32_t);
+                num_written  += printf_internal_Hex(val);
+
+                break;
+            }
+            case '%':
+            {
+                (void)srv_hal_WriteDebugChar('%');
+
+                num_written++;
+                break;
+            }
+            default:
+                break;
+            }
+
+            index++;
+        }
+        else
+        {
+            /* Just write the character out */
+            (void)srv_hal_WriteDebugChar(format[index]);
+
+            num_written++;
+            index++;
+        }
+    }
+
+    return num_written;
+}
+
+int printf(const char* format, ...)
+{
+    __builtin_va_list arg_list;
+
+    __builtin_va_start(arg_list, format);
+    const int ret = printf_internal(format, &arg_list);
+    __builtin_va_end(arg_list);
+
+    return ret;
+}
---
+++ kernel/kstdlib/stdio.h
@@ -0,0 +1,23 @@
+/****************************************************************
+ * @file    stdio.h
+ * @brief   Kernel Standard Library stdio.h implementation
+ *
+ * @copyright SPDX-License-Identifier: GPL-3.0-or-later
+ ****************************************************************/
+
+#ifndef STDIO_H
+#define STDIO_H
+
+/**
+ * @brief Kernel internal printf function
+ *
+ * @param[in] format The format string to write
+ *
+ * @note This function implicitly writes to the Kernel's debug console using the
+ *       underlying Architecture HAL!
+ *
+ * @return The number of bytes written
+ */
+[[gnu::format(printf, 1, 2)]] int printf(const char* format, ...);
+
+#endif
