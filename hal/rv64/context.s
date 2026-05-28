2077a5f44d756eade01bbb3cfb240f451b935ec5
Jesse Buhagiar <jooster669@gmail.com>
2026-05-28 11:44:12 +0000

    fix: Save correct SP in context save, and fix function return

--- hal/rv64/context.s
+++ hal/rv64/context.s
@@ -12,15 +12,19 @@
 
 srv_hal_SaveCPUContext:
     # Increase the caller's stack to save the context directly
-    addi sp, sp, -256
+    addi sp, sp, -288
 
     # Start saving registers
     sd zero,    0(sp)
 	sd ra,      8(sp)
-	sd sp,      16(sp)
+	sd t0,      40(sp) # We save t0 first so we can use it as scratch
+
+	# Get the pre decremented sp
+	addi t0, sp, 288
+	sd t0,      16(sp)
+
 	sd gp,      24(sp)
 	sd tp,      32(sp)
-	sd t0,      40(sp)
 	sd t1,      48(sp)
 	sd t2,      56(sp)
 	sd s0,      64(sp)
@@ -48,12 +52,25 @@ srv_hal_SaveCPUContext:
 	sd t5,      240(sp)
 	sd t6,      248(sp)
 
+	# Read out the supervisor registers
+	csrr t0, sepc
+	csrr t1, scause
+	csrr t2, stval
+	csrr t3, sstatus
+
+	sd t0, 256(sp)
+	sd t1, 264(sp)
+	sd t2, 272(sp)
+	sd t3, 280(sp)
+
     mv a0, sp
 
     # Branch to the internal function to actually save it to the control block
     call hal_SaveContextToProcControlBlock
 
-    # Don't leak stack space!
-    addi sp, sp, 256
+    # Restore original ra clobbered by call
+    ld ra, 8(sp)
+    addi sp, sp, 288
 
-	jr ra
+	# Get out of here
+    jr ra
